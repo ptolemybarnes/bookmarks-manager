b7b1d229eb8b678fb147bfc5ac26bb482e129740
Ptolemy <ptolemybarnes@gmail.com>
2015-01-15 18:56:20 +0000

    User password will only reset if passwords match

--- app/controllers/password_recovery.rb
+++ app/controllers/password_recovery.rb
@@ -7,9 +7,9 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users/password_recovery' do
-    User.raise_on_save_failure = true 
-     user = User.first(:email => params[:email])
+    user = User.first(:email => params[:email])
     token = (1..64).map { ('A'..'Z').to_a.sample }.join
+
     user.update(password_token: token, password_token_timestamp: Time.now)
     recovery_url = url_for("/users/password_recovery/#{user.password_token}", :full)
 
@@ -23,9 +23,20 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/password_recovery/:token' do
-    # byebug
-    user = User.first( :password_token => params[:token] )
-    "Hello," + user.email
+    @user = User.first( :password_token => params[:token] )
+    @token= params[:token]
+    erb :"users/reset_password"
+  end
+
+  post '/users/reset_password' do
+    @user = User.first( :password_token => params[:token] )
+    byebug
+    if User.update(:password              => params[:password], :password_confirmation => params[:password_confirmation])
+       session[:user_id] = @user.id
+       redirect to('/')
+    else
+       "#{@user.errors.full_messages}"
+    end
   end
 
 end
